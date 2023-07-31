edd98f82826ea9f84c3a0f98c8e6fd7222d77911
cmaynard <chrissy.swe.maynard@gmail.com>
2023-07-31 17:05:52 -0400

    implement a method to insert a node into a SinglyLinkedList

--- src/LinkedLists/SinglyLinkedList/Main.java
+++ src/LinkedLists/SinglyLinkedList/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args){
         SinglyLinkedList sLL = new SinglyLinkedList();
         sLL.createSinglyLinkedList(5);
-        System.out.println(sLL.head.value);
+        System.out.println(sLL.head.value); // prints 5
+
+        sLL.insertIntLinkedList(6,0);
+        System.out.println(sLL.head.value); // prints 6
+        System.out.println(sLL.head.nextNodeReference.value); // prints 5
     }
 }
--- src/LinkedLists/SinglyLinkedList/SinglyLinkedList.java
+++ src/LinkedLists/SinglyLinkedList/SinglyLinkedList.java
@@ -18,4 +18,38 @@ public class SinglyLinkedList {
         size += 1;
         return head;
     }
+
+    // Insert a node at any given location
+    public void insertIntLinkedList(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+        // check if the linkedlist is already created
+        // we will know this if head is null
+        // if head is null, create a linked list
+        if(head == null){
+            // create a singly linked list with at least one node
+            createSinglyLinkedList(nodeValue);
+            return;
+        } else if(location == 0){
+            // if a node already exists, go ahead and re-assign the references
+            node.nextNodeReference = head;
+            head = node;
+        } else if (location >= size) { // add the node to the end of the list
+            node.nextNodeReference = null;
+            tail.nextNodeReference = node;
+            tail = node;
+
+        } else { // add the node to a specified location
+            Node tempNode = head;
+            int index = 0;
+            while(index < location -1){ // loop thru the list to find where we are inserting the node
+                tempNode = tempNode.nextNodeReference;
+                index++;
+            }
+            Node nextNode = tempNode.nextNodeReference;
+            tempNode.nextNodeReference = node;
+            node.nextNodeReference = nextNode;
+        }
+        size += 1;
+    }
 }
